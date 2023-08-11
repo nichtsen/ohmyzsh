d9f6300e6b26fc1ecf550800f89dee69a5e41c62
nichtsen <hobblong@163.com>
2023-08-11 09:50:22 +0800

    add mirror repo

--- tools/install.sh
+++ tools/install.sh
@@ -67,8 +67,8 @@ zdot="${ZDOTDIR:-$HOME}"
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 # Default settings
-REPO=${REPO:-ohmyzsh/ohmyzsh}
-REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+REPO=${REPO:-mirrors/oh-my-zsh}
+REMOTE=${REMOTE:-https://gitee.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
 # Other options
